44e3e0df070a4c75908006a0cd8c7fa0369cf49c
yoonseo choi <choiyoonseo02@gmail.com>
2023-12-14 16:49:42 +0900

    feat: 기능 선택 입력값 검증 및 예외처리

--- src/main/java/pairmatching/domain/Controller.java
+++ src/main/java/pairmatching/domain/Controller.java
@@ -1,5 +1,6 @@
 package pairmatching.domain;
 
+import java.util.Objects;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
 
@@ -13,9 +14,25 @@ public class Controller {
     }
 
     public void pairMatchingLogic() {
-        String function = inputView.inputFunction();
+        inputFuction();
         System.out.println();
         outputView.courseAndMission();
         String courseAndLevelAndMission = inputView.inputCourseAndLevelAndMission();
+    }
+
+    private void inputFuction() {
+        try {
+            String function = inputView.inputFunction();
+            validateInputFuction(function);
+        } catch (IllegalArgumentException e) {
+            System.out.println();
+        }
+    }
+
+    private void validateInputFuction(String function) {
+        if (!Objects.equals(function, "1") && !Objects.equals(function, "2") && !Objects.equals(function, "3")
+                && !Objects.equals(function, "Q")) {
+            throw new IllegalArgumentException();
+        }
     }
 }
